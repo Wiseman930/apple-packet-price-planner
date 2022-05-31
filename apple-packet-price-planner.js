23bb68892516cedcdacc508bc6a7aa04a1ad5da1
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-05-31 12:06:10 +0200

    update

---
+++ apple-packet-price-planner.js
@@ -0,0 +1,41 @@
+function applePricePlan(){
+
+
+
+    function costPriceBox(boxPrice){
+
+    }
+    function AppleNumberInBox(AppleBox){
+
+    }
+    function packetSizeApple(){
+
+    }
+    function requiredprofit(){
+
+    }
+    function NumberofPackets(){
+
+    }
+    function pricePerApple(){
+
+    }
+    function pricePerPacket(){
+
+    }
+    function recommendPacketPrice(){
+
+    }
+    return {
+        costPriceBox,
+        AppleNumberInBox,
+        packetSizeApple,
+        requiredprofit,
+        NumberofPackets,
+        pricePerApple,
+        pricePerPacket,
+        recommendPacketPrice
+    }
+
+
+}
